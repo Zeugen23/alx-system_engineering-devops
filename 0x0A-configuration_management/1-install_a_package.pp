7a5e759269a37849e60e3ddb8ee29b22e169179e
Zeugen23 <ulrichparvel@gmail.com>
2024-04-01 01:52:14 -0700

    all best

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# install an especific version of flask (2.1.0)
-package {'flask':
+# installs the package flask
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
